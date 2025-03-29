70c4aadc6101d4335040675305834c665ddbba28
5°INA ITT Montani <113601099+cere04@users.noreply.github.com>
2025-03-29 04:07:43 +0100

    aggiornamento webapp

--- estensione/popup.js
+++ estensione/popup.js
@@ -1,25 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  function updateStats() {
-    chrome.runtime.sendMessage({ action: "getDataUsage" }, (response) => {
-      let statsElement = document.getElementById("stats");
-      let totalsElement = document.getElementById("totals");
-      statsElement.innerHTML = "";
-
-      if (response && response.sites.length > 0) {
-        response.sites.forEach((site) => {
-          let listItem = document.createElement("li");
-          // listItem.innerText = `\nDati: ${site.dataUsed.toFixed(2)} MB\nCO2: ${site.co2Emitted.toFixed(4)} g\nTempo: ${site.timeSpent.toFixed(2)} sec`;
-          // statsElement.appendChild(listItem);
-        });
-        
-        totalsElement.innerText = `Dati totali: ${response.totalDataUsed.toFixed(2)} MB\nCO2 totale: ${response.totalCO2Emitted.toFixed(4)} g\nTempo totale: ${response.totalTimeSpent.toFixed(2)} sec`;
-      } else {
-        statsElement.innerText = "Nessun dato disponibile";
-        totalsElement.innerText = "Nessun dato disponibile";
-      }
-    });
-  }
-
-  updateStats();
-  setInterval(updateStats, 1000); // Aggiorna ogni 5 secondi
+document.getElementById('inviaDati').addEventListener('click', function() {
+  const valoreDaInviare = "aaa"; // Sostituisci con il valore desiderato
+  console.log(valoreDaInviare)
+  
+  fetch('localhost/api', { // Sostituisci con l'URL del tuo database
+    method: 'POST',
+    headers: {  
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ valore: valoreDaInviare })
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Dati inviati con successo!');
+    } else {
+      console.error('Errore durante l\'invio dei dati:', response.status);
+    }
+  })
+  .catch(error => {
+    console.error('Errore di rete:', error);
+  });
 });
